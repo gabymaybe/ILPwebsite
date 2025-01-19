e2e7f03cb48622355830dafb9f0ac7615bf9e3be
Gabe Meredith <gabriel.b.meredith@gmail.com>
2025-01-19 14:18:09 -0500

    added icons to hero section

--- root/gif.js
+++ root/gif.js
@@ -6,6 +6,6 @@ let isPlaying = false; // Track whether the GIF is playing
 playButton.addEventListener("click", () => {
   // Update the `src` attribute of the image
   gif.src = isPlaying ? "Images/rome.jpg" : "Images/Colloseumgif.gif";
-  playButton.textContent = isPlaying ? "Play GIF" : "Stop GIF";
+  playButton.textContent = isPlaying ? "Click me to combine the Colosseum with the starry night!" : "Stop GIF";
   isPlaying = !isPlaying; // Toggle the play state
 });
